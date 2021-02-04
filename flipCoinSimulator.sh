9ef8455f67297feb164a6e3c08eee29653b8838a
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-04 23:26:16 +0530

    Added logic for displaying how many times heads won as well how many times tails won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,9 +2,21 @@
 
 echo "Welcome to Flip Coin Simulator Problem"
 
-if [[ $((RANDOM%2)) -eq 1 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+n=0
+heads=0
+tails=0
+while [ $n -lt 10 ]
+do
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		echo "Heads"
+		heads=$(($heads+1))
+	else
+		echo "Tails"
+		tails=$(($tails+1))
+	fi
+	n=$(($n+1))
+done
+echo "Heads won: " $heads
+echo "Tails won: " $tails
+
